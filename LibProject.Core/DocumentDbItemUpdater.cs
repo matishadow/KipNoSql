1e17f710214ad3dd7f8f7144361bf71aa4ab1590
matishadow <matishadow@gmail.com>
2017-04-22 18:48:55 +0200

    Add updater class

---
+++ LibProject.Core/DocumentDbItemUpdater.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using LibProject.Core;
+using LibProject.Interfaces;
+using LibProject.Models;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+
+namespace LibProject.Core
+{
+    public class DocumentDbItemUpdater<T> : DocumentDbBase, IDocumentDbItemUpdater<T> where T : class
+    {
+        public DocumentDbItemUpdater(DocumentDbCredentials documentDbCredentials, IDocumentClient documentClient)
+            : base(documentDbCredentials, documentClient)
+        {
+        }
+
+        public async Task<Document> UpdateItemAsync(Guid id, T item)
+        {
+            return
+                await
+                    DocumentClient.ReplaceDocumentAsync(
+                        UriFactory.CreateDocumentUri(DocumentDbCredentials.DatabaseId,
+                            DocumentDbCredentials.CollectionId, id.ToString()), item);
+        }
+
+        public Document UpdateItem(Guid id, T item)
+        {
+            return UpdateItemAsync(id, item).Result;
+        }
+    }
+}
---
+++ LibProject.Interfaces/IDocumentDbItemUpdater.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+
+namespace LibProject.Interfaces
+{
+    public interface IDocumentDbItemUpdater<in T>
+    {
+        Task<Document> UpdateItemAsync(Guid id, T item);
+        Document UpdateItem(Guid id, T item);
+    }
+}
