bd7199065ee8e9a97ccc6a0319a1fc538841a8f4
matishadow <matishadow@gmail.com>
2017-04-22 18:55:00 +0200

    Add creator class

---
+++ LibProject.Interfaces/IDocumentDbItemCreator.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+
+namespace LibProject.Interfaces
+{
+    public interface IDocumentDbItemCreator<in T>
+    {
+        Task<Document> CreateItemAsync(T item);
+        Document CreateItem(T item);
+    }
+}
