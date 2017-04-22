901c48c464874f430d1768c2d9f1d4978950d013
matishadow <matishadow@gmail.com>
2017-04-22 18:43:50 +0200

    Add deletion class

---
+++ LibProject.Core/DocumentDbItemDeleter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using LibProject.Interfaces;
+using LibProject.Models;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+
+namespace LibProject.Core
+{
+    public class DocumentDbItemDeleter : DocumentDbBase, IDocumentDbItemDeleter
+    {
+        public DocumentDbItemDeleter(DocumentDbCredentials documentDbCredentials, IDocumentClient documentClient)
+            : base(documentDbCredentials, documentClient)
+        {
+        }
+
+        public async Task DeleteItemAsync(Guid id)
+        {
+            await
+                DocumentClient.DeleteDocumentAsync(UriFactory.CreateDocumentUri(DocumentDbCredentials.DatabaseId,
+                    DocumentDbCredentials.CollectionId, id.ToString()));
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            DeleteItemAsync(id).RunSynchronously();
+        }
+    }
+}
