62c3af39d788d8c655058f3402bd4519891826ef
matishadow <matishadow@gmail.com>
2017-06-17 23:15:00 +0200

    add web project

---
+++ Kip.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kip.Web.Startup))]
+namespace Kip.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
