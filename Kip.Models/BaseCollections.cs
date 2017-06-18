d690fc1e746fff7212634fe9c50556d1eca02984
matishadow <matishadow@gmail.com>
2017-06-18 18:06:03 +0200

    add home view

---
+++ Kip.Models/BaseCollections.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Kip.Models.Base;
+
+namespace Kip.Models
+{
+    public class BaseCollections
+    {
+        public IEnumerable<Book> Books { get; set; }
+        public IEnumerable<EBook> EBooks { get; set; }
+        public IEnumerable<GameDisc> GameDiscs { get; set; }
+        public IEnumerable<Magazine> Magazines { get; set; }
+        public IEnumerable<MovieDvd> MovieDvds { get; set; }
+        public IEnumerable<MusicCd> MusicCds { get; set; }
+    }
+}
--- Kip.Web/Controllers/HomeController.cs
+++ Kip.Web/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Kip.Models;
+using Kip.Models.Base;
 
 namespace Kip.Web.Controllers
 {
@@ -10,20 +12,20 @@ namespace Kip.Web.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var m = new List<Book>()
+            {
+                new Book() {Author = "A", ISBN = "b", PageCount = 4, Title = "c"}
+            };
+            return View(new BaseCollections(){Books = m});
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult PresentationLink()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
